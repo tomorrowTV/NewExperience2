ae990f0f6c62db775e7daeb48f671a3662de1f75
tomorrowTV <146773400+tomorrowTV@users.noreply.github.com>
2023-11-04 17:36:39 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,30 +1,9 @@
-// ... Your existing code ...
-
 document.addEventListener('DOMContentLoaded', function () {
     const videoPlayerContainer = document.getElementById('videoPlayerContainer');
-    const videoElement = document.createElement('video');
-    videoElement.id = 'videoPlayer';
-    videoElement.controls = true; // Add controls for user interaction
-    videoElement.setAttribute('playsinline', ''); // Add playsinline attribute
-    videoElement.preload = 'auto'; // Set preload attribute to 'auto' for video
-    videoPlayerContainer.appendChild(videoElement);
+    let videoElement; // Declare videoElement variable
 
-    // Create an audio element for the background audio
-    const backgroundAudio = document.createElement('audio');
-    backgroundAudio.src = 'wwwroot/assets/Song.m4a'; // Update this to the relative path of your audio file
-    backgroundAudio.preload = 'auto';
-    backgroundAudio.loop = true; // Set the loop attribute to true for continuous playback
-    backgroundAudio.load();
-    document.body.appendChild(backgroundAudio);
-
-    const canvas = document.createElement('canvas');
-    canvas.width = 960;
-    canvas.height = 540;
-    document.body.appendChild(canvas);
-
-    let currentVideoIndex = 0;
-    const timerInterval = 100; // 100 ms
-    let audioStarted = false;
+    // Create an array to store preloaded video elements
+    const preloadedVideos = [];
 
     // Define the videoArray with the video paths
     const videoArray = [
@@ -37,47 +16,44 @@ document.addEventListener('DOMContentLoaded', function () {
         // Add more video filenames as needed
     ];
 
-    // Preload videos using PreloadJS
-    const preload = new createjs.LoadQueue();
-
-    // Define the video paths for preloading
-    const videoPaths = videoArray.map(path => ({ src: path, type: createjs.Types.VIDEO }));
-
-    preload.loadManifest(videoPaths);
-
-    preload.on('fileload', function (event) {
-        // Handle preloaded video element
-        const preloadedVideo = event.result;
-        // You can use the preloadedVideo element in your application as needed
+    // Preload all videos in advance
+    videoArray.forEach(videoPath => {
+        const video = document.createElement('video');
+        video.src = videoPath;
+        video.preload = 'auto';
+        video.setAttribute('playsinline', ''); // Add playsinline attribute for mobile devices
+        preloadedVideos.push(video);
     });
 
+    // Create an audio element for the background audio
+    const backgroundAudio = document.createElement('audio');
+    backgroundAudio.src = 'wwwroot/assets/Song.m4a'; // Update this to the relative path of your audio file
+    backgroundAudio.preload = 'auto';
+    backgroundAudio.loop = true; // Set the loop attribute to true for continuous playback
+    backgroundAudio.load();
+    document.body.appendChild(backgroundAudio);
+
+    // Function to play video by index
     function playVideoByIndex(index) {
-        videoElement.pause();
-        videoElement.src = videoArray[index];
-        videoElement.currentTime = audioPlayer.currentTime;
-
-        // ... Other video handling code ...
-
-        currentVideoIndex = index;
-    }
-
-    function startAudio() {
-        // Start the background audio
-        backgroundAudio.play().catch(error => {
-            console.error('Audio playback error:', error.message);
-        });
-
-        audioStarted = true;
-    }
-
-    document.addEventListener('click', () => {
-        if (!audioStarted) {
-            startAudio();
+        // Pause the current video
+        if (videoElement) {
+            videoElement.pause();
+            videoPlayerContainer.removeChild(videoElement);
         }
 
-        const nextIndex = (currentVideoIndex + 1) % videoArray.length;
-        playVideoByIndex(nextIndex);
+        // Use the preloaded video element for the new video
+        const newVideoElement = preloadedVideos[index];
+        videoPlayerContainer.appendChild(newVideoElement);
 
-        // ... The rest of your code ...
-    });
+        // Update the reference to the current video element
+        videoElement = newVideoElement;
+
+        // Start playback
+        videoElement.currentTime = backgroundAudio.currentTime;
+        videoElement.play().catch(error => {
+            console.error('Video playback error:', error.message);
+        });
+    }
+
+    // ... Rest of your code ...
 });

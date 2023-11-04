abf2a07ad47b00942099c0969abd86040bf29c8f
tomorrowTV <146773400+tomorrowTV@users.noreply.github.com>
2023-11-04 17:05:21 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// ... Your existing code ...
+
 document.addEventListener('DOMContentLoaded', function () {
     const videoPlayerContainer = document.getElementById('videoPlayerContainer');
     const videoElement = document.createElement('video');
@@ -7,11 +9,13 @@ document.addEventListener('DOMContentLoaded', function () {
     videoElement.preload = 'auto'; // Set preload attribute to 'auto' for video
     videoPlayerContainer.appendChild(videoElement);
 
-    const audioPlayer = document.createElement('audio');
-    audioPlayer.src = 'wwwroot/assets/Song.m4a'; // Update this to the relative path of your audio file
-    audioPlayer.preload = 'auto';
-    audioPlayer.load();
-    document.body.appendChild(audioPlayer);
+    // Create an audio element for the background audio
+    const backgroundAudio = document.createElement('audio');
+    backgroundAudio.src = 'wwwroot/assets/Song.m4a'; // Update this to the relative path of your audio file
+    backgroundAudio.preload = 'auto';
+    backgroundAudio.loop = true; // Set the loop attribute to true for continuous playback
+    backgroundAudio.load();
+    document.body.appendChild(backgroundAudio);
 
     const canvas = document.createElement('canvas');
     canvas.width = 960;
@@ -52,37 +56,28 @@ document.addEventListener('DOMContentLoaded', function () {
         videoElement.src = videoArray[index];
         videoElement.currentTime = audioPlayer.currentTime;
 
-        videoElement.addEventListener('timeupdate', () => {
-            // Update audio playback time to match video time
-            audioPlayer.currentTime = videoElement.currentTime;
-        });
-
-        videoElement.addEventListener('ended', () => {
-            videoElement.currentTime = 0;
-            videoElement.play();
-        });
-
-        videoElement.play().catch(error => {
-            console.error('Video playback error:', error.message);
-        });
+        // ... Other video handling code ...
 
         currentVideoIndex = index;
     }
 
     function startAudio() {
-        audioPlayer.load();
-        audioPlayer.play().catch(error => {
+        // Start the background audio
+        backgroundAudio.play().catch(error => {
             console.error('Audio playback error:', error.message);
         });
+
+        audioStarted = true;
     }
 
     document.addEventListener('click', () => {
         if (!audioStarted) {
             startAudio();
-            audioStarted = true;
         }
 
         const nextIndex = (currentVideoIndex + 1) % videoArray.length;
         playVideoByIndex(nextIndex);
+
+        // ... The rest of your code ...
     });
 });

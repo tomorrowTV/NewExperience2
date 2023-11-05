cdd9bde86e8f3887f24a3f8ab35f56b0ae2bee38
tomorrowTV <146773400+tomorrowTV@users.noreply.github.com>
2023-11-04 21:58:41 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     const videoPlayerContainer = document.getElementById('videoPlayerContainer');
+    let videoElement; // Declare videoElement variable
+
+    // Create an array to store preloaded video elements
+    const preloadedVideos = [];
+
+    // Define the videoArray with the video paths
     const videoArray = [
         'wwwroot/videos/SW1.mp4',
         'wwwroot/videos/SW2.mp4',
@@ -10,34 +16,51 @@ document.addEventListener('DOMContentLoaded', function () {
         // Add more video filenames as needed
     ];
 
-    const audioPlayer = document.createElement('audio');
-    audioPlayer.src = 'wwwroot/assets/Song.m4a'; // Update this to the relative path of your audio file
-    audioPlayer.preload = 'auto';
-    audioPlayer.load();
-    document.body.appendChild(audioPlayer);
-
-    let currentVideoIndex = 0;
-    let videoElement = document.createElement('video');
-    videoElement.id = 'videoPlayer';
-    videoElement.controls = true;
-    videoElement.setAttribute('playsinline', '');
-    videoElement.preload = 'auto';
-    videoElement.src = videoArray[currentVideoIndex];
-
-    videoElement.addEventListener('ended', () => {
-        currentVideoIndex = (currentVideoIndex + 1) % videoArray.length;
-        videoElement.src = videoArray[currentVideoIndex];
-        videoElement.currentTime = 0;
-        videoElement.play();
+    // Preload all videos in advance
+    videoArray.forEach(videoPath => {
+        const video = document.createElement('video');
+        video.src = videoPath;
+        video.preload = 'auto';
+        video.setAttribute('playsinline', ''); // Add playsinline attribute for mobile devices
+        preloadedVideos.push(video);
     });
 
-    videoPlayerContainer.appendChild(videoElement);
+    // Create an audio element for the background audio
+    const backgroundAudio = document.createElement('audio');
+    backgroundAudio.src = 'wwwroot/assets/Song.m4a'; // Update this to the relative path of your audio file
+    backgroundAudio.preload = 'auto';
+    backgroundAudio.loop = true; // Set the loop attribute to true for continuous playback
+    backgroundAudio.load();
+    document.body.appendChild(backgroundAudio);
 
-    document.addEventListener('click', () => {
-        if (!audioPlayer.paused) {
-            audioPlayer.play().catch(error => {
-                console.error('Audio playback error:', error.message);
-            });
+    // Function to play video by index
+    function playVideoByIndex(index) {
+        // Pause the current video
+        if (videoElement) {
+            videoElement.pause();
+            videoPlayerContainer.removeChild(videoElement);
         }
+
+        // Use the preloaded video element for the new video
+        const newVideoElement = preloadedVideos[index];
+        videoPlayerContainer.appendChild(newVideoElement);
+
+        // Update the reference to the current video element
+        videoElement = newVideoElement;
+
+        // Start playback
+        videoElement.currentTime = backgroundAudio.currentTime;
+        videoElement.play().catch(error => {
+            console.error('Video playback error:', error.message);
+        });
+    }
+
+    // Add a click event listener to play audio on user interaction
+    document.addEventListener('click', () => {
+        backgroundAudio.play().catch(error => {
+            console.error('Audio playback error:', error.message);
+        });
     });
+
+    // ... Rest of your code ...
 });

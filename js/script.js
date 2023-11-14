3dbb2590c69b72c99a065bd1e7a1d055e859aa17
tomorrowTV <146773400+tomorrowTV@users.noreply.github.com>
2023-11-13 21:26:18 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     const videoPlayerContainer = document.getElementById('videoPlayerContainer');
-    const loadingScreen = document.getElementById('loadingScreen');
-    const loadingText = document.getElementById('loadingText');
-    const loadingGif = document.getElementById('loadingGif');
+    const loadingBar = document.getElementById('loadingBar');
+    const loadingScreen = document.getElementById('loadingBarContainer');
+    const loadingText = document.getElementById('loadingText'); // Add this line to get the loading text element
 
     let currentVideoIndex = 0;
     let audioPlaying = false;
@@ -28,6 +28,11 @@ document.addEventListener('DOMContentLoaded', function () {
     // Preload assets with progress tracking
     preload.loadManifest(assetsToLoad);
 
+    // Add an event listener for progress updates during loading
+    preload.on('progress', function (event) {
+        loadingBar.style.width = (event.progress * 55) + '%';
+    });
+
     // Add an event listener for when each asset is loaded
     preload.on('fileload', function (event) {
         const asset = event.item.src;
@@ -41,9 +46,8 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         if (preloadedVideos.length === assetsToLoad.length - 1) {
-            // All assets are preloaded, hide loading screen and start the game
-            loadingScreen.style.display = 'none';
-            loadingText.style.display = 'none';
+            // All videos are preloaded, hide loading bar and start the game
+            loadingBar.style.display = 'none';
             startGame();
         }
     });
@@ -93,7 +97,5 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // Change loading text to "Click" when the game starts
         loadingText.textContent = 'Click';
-
-        console.log('Game started, loading text changed to "Click"');
     }
 });
